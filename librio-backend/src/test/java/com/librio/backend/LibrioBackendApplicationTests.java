54d8bd0b3a2d933b7b817344ea5987bc857e6a13
clementvenot <clement.venooot@gmail.com>
2025-12-24 15:58:21 +0100

    initialisation app

---
+++ librio-backend/src/test/java/com/librio/backend/LibrioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ librio-frontend/src/test/java/com/librio/frontend/LbrioFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librio.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbrioFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
