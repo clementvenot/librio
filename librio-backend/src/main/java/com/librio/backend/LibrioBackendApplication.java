c618f6c021fa8c083bcb29506c4c28efd486aace
Clement Venot <clement.venooot@gmail.com>
2025-12-26 00:36:29 +0100

    test connexion git over

--- librio-backend/src/main/java/com/librio/backend/LibrioBackendApplication.java
+++ librio-backend/src/main/java/com/librio/backend/LibrioBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibrioBackendApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(LibrioBackendApplication.class, args); 
 		
 	}
